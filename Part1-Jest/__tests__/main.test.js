1d3cba8c1862a7fe1aed28eddfe8650bc84188d0
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-25 21:31:57 -0800

    update

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -2,15 +2,15 @@ var formatVolumeIconPath = require('../assets/scripts/main');
 
 describe('Volume icon level is ', () => {
     test('3', () => {
-        expect(formatVolumeIconPath(69).toContain("3"));
+        expect(formatVolumeIconPath(69)).toContain("3");
     });
     test('2', () => {
-        expect(formatVolumeIconPath(40).toContain("3"));
+        expect(formatVolumeIconPath(40)).toContain("3");
     });
     test('1', () => {
-        expect(formatVolumeIconPath(8).toContain("3"));
+        expect(formatVolumeIconPath(8)).toContain("3");
     });
     test('0', () => {
-        expect(formatVolumeIconPath(0).toContain("3"));
+        expect(formatVolumeIconPath(0)).toContain("3");
     });
 });
